5f46042319bb5f1f69f4b328527ff3f134fa7452
Sam198141 <gril1111139@gmail.com>
2023-06-28 17:28:24 +0800

    no message

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -8,7 +8,7 @@ class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
-		System.out.println("========================>dev test1");
+		System.out.println("========================>dev test2");
 	}
 
 }
